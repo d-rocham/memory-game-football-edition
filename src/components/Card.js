79a3bc88ad2c030585da2c468054f9dba44498b6
d-rocham <d_rocham@outlook.com>
2022-01-14 12:54:34 -0500

    Shuffle cards on click

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,12 +1,10 @@
-let club = "boca";
-
-const Card = () => {
+const Card = ({ club, onSelection }) => {
     return (
-        <div className="card flex flex-col items-center justify-evenly shadow-md bg-white">
+        <div className="card flex flex-col items-center justify-evenly shadow-md bg-white" onClick={() => onSelection(club.id)}>
             <div className="w-3/4 h-3/4">
-                <img src={process.env.PUBLIC_URL + `assets/laf/${club}.png`} alt="" className="object-scale-down" />
+                <img src={process.env.PUBLIC_URL + `assets/laf/${club.filename}.png`} alt="" className="object-scale-down" />
             </div>
-            <p className="font-urbanist text-2xl text-center">Boca Juniors</p>
+            <p className="font-urbanist text-2xl text-center">{club.name}</p>
         </div >
     )
 
--- src/components/Gameboard.js
+++ src/components/Gameboard.js
@@ -1,18 +1,64 @@
 import Card from "./Card";
+import { useState } from "react";
 
-const Gameboard = () => {
+const Gameboard = ({ selectedLeague, level }) => {
+    const getClubs = () => {
+
+        const getRandomID = () => {
+            return Math.floor(Math.random() * 26);
+        }
+
+        const generateRandomClubList = () => {
+            let levelIds = [];
+
+            for (let i = 0; i < level; i++) {
+                // Generate list of random, unique clubs w. length determined by level
+                let randomid;
+
+                // Prevent repeated id's in list
+                do {
+                    randomid = getRandomID();
+                } while (levelIds.includes(randomid));
+
+                levelIds.push(randomid)
+            }
+
+            return levelIds;
+        }
+
+        return generateRandomClubList().map(id => selectedLeague[id]);
+    }
+
+    const [clubs, setClubs] = useState(() => getClubs())
+
+    const reArrangeClubs = () => {
+
+        let arr = [...clubs];
+
+        let currentIndex = clubs.length, randomIndex;
+
+        while (currentIndex != 0) {
+            randomIndex = Math.floor(Math.random() * clubs.length);
+            currentIndex--;
+
+            [arr[currentIndex], arr[randomIndex]] = [arr[randomIndex], arr[currentIndex]];
+
+            setClubs([...arr]);
+        }
+    }
+
+    const clickedItems = [];
+
+    const updateClicked = (id) => {
+        clickedItems.includes(id) ? console.log("Restart game!") : clickedItems.push(id);
+
+        reArrangeClubs(); //TODO: This should only happen if clicked card isn't in clickedItems
+    }
+
     return (
+        /* TODO: Grid size depends on current level. */
         <div className="col-start-3 col-span-8 h-screen p-2 bg-blue-50 grid grid-cols-4 grid-rows-2 gap-2 justify-items-stretch items-stretch">
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-
-
+            {clubs.map(club => <Card club={club} key={club.id} onSelection={updateClicked} />)}
         </div>
     )
 }
